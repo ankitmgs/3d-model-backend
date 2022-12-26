a5c65ec6bb044bc23fad812f7a828c0838da3d31
Ankit Gupta <85329746+ankitmgs@users.noreply.github.com>
2022-12-26 14:14:39 +0530

    ..

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -16,6 +16,19 @@ router.post("/add", (req, res) => {
     });
 });
 
+router.get("/getall", (req, res) => {
+
+
+  Model.find({})
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post("/check-login", (req, res) => {
   console.log(req.body);
 
